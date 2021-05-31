d813ad918f5fde2f9c7fe1b048f32a4aecd7a5d0
Sid20-rgb <siddharthashakya37@gmail.com>
2021-05-31 12:00:07 +0545

    initial commit

---
+++ one.py
@@ -0,0 +1,13 @@
+#Write a program that takes three numbers and prints their sum. Every number is given on a seperate line.
+
+num1 = int(input("Enter 1st number:"))
+
+num2 = int(input("Enter 2nd number:"))
+
+num3 = int(input("Enter 3rd number:"))
+
+sum = num1 + num2 + num3
+
+print(sum)
+
+print(f"The sum of three numbers is {sum}")
