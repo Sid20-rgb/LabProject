f4c2b98ff3a60a1c1ec0914b1fb741a4af0447a5
Sid20-rgb <siddharthashakya37@gmail.com>
2021-06-03 14:39:52 +0545

    initial commit

--- labone/one.py
+++ labone/one.py
@@ -1,5 +1,12 @@
-
 num1 = int(input("Enter 1st number:"))
+
+num2 = int(input("Enter 2nd number:"))
+
+num3 = int(input("Enter 3rd number:"))
+
+sum = num1+num2+num3
+
+print(f"The sum is {sum}")num1 = int(input("Enter 1st number:"))
 
 num2 = int(input("Enter 2nd number:"))
 
