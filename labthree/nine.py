2e4662216cfe88c7669a750146ffade17c3d85dd
Sid20-rgb <75927865+Sid20-rgb@users.noreply.github.com>
2021-06-30 20:05:44 +0545

    Delete nine.py

--- labthree/nine.py
+++
@@ -1,2 +0,0 @@
-'''.9 Write a Python function that checks whether a passed string is palindrome or not.Note: A palindrome is a word, phrase,
-or sequence that reads the same backward as forward, e.g., madam or nurses run.'''
