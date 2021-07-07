092c9d6913b02cb164941cd9988127dd2dd1dde8
Sid20-rgb <siddharthashakya37@gmail.com>
2021-07-07 22:03:06 +0545

    guess_num

--- labfour/one.py
+++ labfour/one.py
@@ -4,5 +4,9 @@
 num = []
 for x in range(1500, 2701):
     if x%7== 0 and x%5 == 0:
+
+        print(x, end= " ")
+
+        '''can be written in this way as well
         num.append(str(x))
-print(','. join(num))
+print(','. join(num))''' 
